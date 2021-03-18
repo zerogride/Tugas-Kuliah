49abaa5ea46f22dc5279ffa975a820c160718bf3
zerogride <69594776+zerogride@users.noreply.github.com>
2021-03-18 15:36:30 +0700

    Create TugasPemogramanPertemuan4.java

---
+++ TugasPemogramanPertemuan4.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+public class javax{
+ public static void main(String [] args){
+ String nama="";
+ nama = JOptionPane.showInputDialog("Ketik nama anda : ");
+ String msg = "Hello"+ nama +"\nSilahkan Gunakan Program ini untuk melihat
+Grade nilai kamu !";
+ JOptionPane.showMessageDialog(null, msg);
+ int
+ hasil=Integer.parseInt(JOptionPane.showInputDialog("Masukan nilai Anda :")
+);
+ {
+ if (hasil>=80) {
+ JOptionPane.showMessageDialog(null,"Anda mendapatkan Grade A");
+ }
+ else if(hasil<=80){
+ JOptionPane.showMessageDialog(null,"Anda mendapatkan Grade B");
+ }
+ else if(hasil<=70){
+ JOptionPane.showMessageDialog(null,"Anda mendapatkan Grade C");
+ }
+ else if(hasil<=60){
+ JOptionPane.showMessageDialog(null,"Anda mendapatkan Grade D");
+ }
+ else if(hasil<=50){
+ JOptionPane.showMessageDialog(null,"Anda mendapatkan Grade E");
+ }
+}
+}
+}
